72269d1cc88299290f833db4e1f73f12b476c8d3
Ram Katru <rkatru@gmail.com>
2026-09-11 16:42:42 -0400

    Reverting 'low' change and will fix properly

--- internal/provider/runpod/types.go
+++ internal/provider/runpod/types.go
@@ -124,7 +124,7 @@ func inStock(status *string) bool {
 		return false
 	}
 	switch strings.ToLower(*status) {
-	case "high", "medium", "low":
+	case "high", "medium":
 		return true
 	}
 	return false
